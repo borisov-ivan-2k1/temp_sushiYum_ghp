a56d9467ce8ff3613bb6369311fca304e3bb317a
Ivan <iborisov@pryaniky.onmicrosoft.com>
2020-08-04 12:44:38 +0300

    build

---
+++ precache-manifest.6283dcfd566ffd7a65286caf730c47f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7763c3669c8267cb10e48cc20c841760",
+    "url": "/temp_sushiYum_ghp/index.html"
+  },
+  {
+    "revision": "e8f35a561822c216b891",
+    "url": "/temp_sushiYum_ghp/static/css/main.c09aed55.chunk.css"
+  },
+  {
+    "revision": "ce9c906a7d6bd757a953",
+    "url": "/temp_sushiYum_ghp/static/js/2.770114fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/temp_sushiYum_ghp/static/js/2.770114fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f35a561822c216b891",
+    "url": "/temp_sushiYum_ghp/static/js/main.194e64d3.chunk.js"
+  },
+  {
+    "revision": "311097740952a5f67ec3",
+    "url": "/temp_sushiYum_ghp/static/js/runtime-main.9bdb2163.js"
+  },
+  {
+    "revision": "98883b9a64257b5a52301cda0093ed68",
+    "url": "/temp_sushiYum_ghp/static/media/mainHeader.98883b9a.png"
+  }
+]);
